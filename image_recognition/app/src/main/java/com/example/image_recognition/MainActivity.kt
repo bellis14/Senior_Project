bc2d66ddfaa73793662c26ea593f9af820b5b23c
TonyVirgin <vir16007@byui.edu>
2022-01-14 21:38:15 -0700

    Added dependencies

--- image_recognition/app/src/main/java/com/example/image_recognition/MainActivity.kt
+++ image_recognition/app/src/main/java/com/example/image_recognition/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.image_recognition
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.VideoView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+    }
+
+    val REQUEST_VIDEO_CAPTURE = 1
+
+    private fun dispatchTakeVideoIntent() {
+        Intent(MediaStore.ACTION_VIDEO_CAPTURE).also { takeVideoIntent ->
+            takeVideoIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE)
+            }
+        }
     }
 }
