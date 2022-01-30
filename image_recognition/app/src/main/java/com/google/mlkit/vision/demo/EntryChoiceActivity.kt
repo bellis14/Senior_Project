f167ddefaa875ed2f8b026f72623093ce504f345
Ben Ellis <ben.ellis1919@gmail.com>
2022-01-29 18:19:18 -0700

    find center of face detecting images

--- image_recognition/app/src/main/java/com/google/mlkit/vision/demo/EntryChoiceActivity.kt
+++ image_recognition/app/src/main/java/com/google/mlkit/vision/demo/EntryChoiceActivity.kt
@@ -18,8 +18,11 @@ package com.google.mlkit.vision.demo
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
+import com.google.mlkit.vision.demo.kotlin.SelectDevice
+
 //import com.google.mlkit.vision.demo.java.ChooserActivity
 
 class EntryChoiceActivity : AppCompatActivity() {
@@ -38,5 +41,10 @@ class EntryChoiceActivity : AppCompatActivity() {
         )
       startActivity(intent)
     }
+  }
+
+  fun openSelectDeviceActivity(view: View) {
+    val intent = Intent(this@EntryChoiceActivity, com.google.mlkit.vision.demo.kotlin.SelectDevice::class.java)
+    startActivity(intent)
   }
 }
