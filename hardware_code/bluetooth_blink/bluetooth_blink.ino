231e02ba65f5555c4748e1404955654252010772
Ben Ellis <ben.ellis1919@gmail.com>
2022-02-02 16:29:24 -0700

    updated ESP32 bluetooth code

--- hardware_code/bluetooth_blink/bluetooth_blink.ino
+++ hardware_code/bluetooth_blink/bluetooth_blink.ino
@@ -1,51 +1,31 @@
 //www.elegoo.com
 
-//#define LED 13    //Define 13 pin for LED
-//bool state = LOW; //The initial state of the function is defined as a low level
-//char getstr;      //Defines a function that receives the Bluetooth character
-
-
-#include <Servo.h>
-#include <SoftwareSerial.h>
-#define servoPin 3
-SoftwareSerial mySerial(1, 0);
-Servo servo;
-int servoPos = 0;
-char Incoming_value = 0;                //Variable for storing Incoming_value
-
-void setup() 
-{
-  Serial.begin(9600);         //Sets the data rate in bits per second (baud) for serial data transmission
-  servo.attach(servoPin);
-  Serial.println("Enter AT commands:");
-  mySerial.begin(9600);
-  //pinMode(13, OUTPUT);        //Sets digital pin 13 as output pin
-}
-void loop()
-{
-  /*if(Serial.available() > 0)  
-  {
-    Incoming_value = Serial.read();      //Read the incoming data and store it into variable Incoming_value
-    Serial.print(Incoming_value);        //Print Value of Incoming_value in Serial monitor
-    Serial.print("\n");        //New line 
-    if(Incoming_value == '1')            //Checks whether value of Incoming_value is equal to 1 
-    {
-      //digitalWrite(13, HIGH);  //If value is 1 then LED turns ON
-      servoLeft();
-    }
-    else if(Incoming_value == '0')       //Checks whether value of Incoming_value is equal to 0
-    {
-      //digitalWrite(13, LOW);   //If value is 0 then LED turns OFF
-      servoRight();
-    }
-  }  */  
-  if (mySerial.available())
-    Serial.write(mySerial.read());
-  if (Serial.available())
-    mySerial.write(Serial.read()); 
+#include "BluetoothSerial.h"
  
-}        
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
 
+BluetoothSerial SerialBT;
+ 
+void setup() {
+  Serial.begin(115200);
+  //Serial.begin(9600);
+  SerialBT.begin("AutoCameraMan"); //Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
+}
+ 
+void loop() {
+  if (Serial.available()) {
+    SerialBT.write(Serial.read());
+  }
+  if (SerialBT.available()) {
+    Serial.write(SerialBT.read());
+  }
+  delay(20);
+}
+
+/*
 void servoRight()
 {
   for (servoPos = 55; servoPos <= 125; servoPos++)
@@ -62,26 +42,4 @@ void servoLeft()
      servo.write(servoPos);
      delay(15);
    }
-}
-/*void setup() {
-  pinMode(LED, OUTPUT);
-  Serial.begin(9600);
-}
-
-//Control LED sub function
-void stateChange() {
-  state = !state; 
-  digitalWrite(LED, state);  
-}
-
-void loop() {
-  if(Serial.available())
-  {
-    //The Bluetooth serial port to receive the data in the function
-    getstr = Serial.read();
-    Serial.print(getstr);
-    if(getstr == 'a'){
-      stateChange();
-    }
-  }
 }*/
