2fe0e293d7378610c2b2c14144d5c5dd21d1e0d3
Ben Ellis <ben.ellis1919@gmail.com>
2022-02-08 14:27:41 -0700

    added servo control on ESP32

--- hardware_code/bluetooth_blink/bluetooth_blink.ino
+++ hardware_code/bluetooth_blink/bluetooth_blink.ino
@@ -1,21 +1,33 @@
 //www.elegoo.com
 
 #include "BluetoothSerial.h"
- 
+#include <Servo_ESP32.h>
+
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
 
 BluetoothSerial SerialBT;
+static const int servoPin = 18; //printed G14 on the board
+Servo_ESP32 servo1;
+int angle =0;
+int angleStep = 5;
+int angleMin =0;
+int angleMax = 180;
  
 void setup() {
   Serial.begin(115200);
-  //Serial.begin(9600);
-  SerialBT.begin("AutoCameraMan"); //Bluetooth device name
+  SerialBT.begin("AutoCameraMan Base"); //Bluetooth device name
   Serial.println("The device started, now you can pair it with bluetooth!");
+  servo1.attach(servoPin);
 }
  
 void loop() {
+  servoRight();
+  delay(2000);
+  servoLeft();
+  delay(2000);
+  
   if (Serial.available()) {
     SerialBT.write(Serial.read());
   }
@@ -25,21 +37,21 @@ void loop() {
   delay(20);
 }
 
-/*
+
 void servoRight()
 {
-  for (servoPos = 55; servoPos <= 125; servoPos++)
-   {
-     servo.write(servoPos);
-     delay(15);
-   }
+   for(int angle = 0; angle <= angleMax; angle +=angleStep) {
+        servo1.write(angle);
+        Serial.println(angle);
+        delay(20);
+    }
 }
 
 void servoLeft()
 {
-  for (servoPos = 125; servoPos >= 55; servoPos--)
-   {
-     servo.write(servoPos);
-     delay(15);
-   }
-}*/
+  for(int angle = 180; angle >= angleMin; angle -=angleStep) {
+        servo1.write(angle);
+        Serial.println(angle);
+        delay(20);
+    }
+}
