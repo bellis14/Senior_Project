0ce3a6590d58c910f0b4284096245f37b56f6634
Ben Ellis <ben.ellis1919@gmail.com>
2022-02-14 16:31:01 -0700

    updates

--- hardware_code/AutoCameraMan/AutoCameraMan.ino
+++ hardware_code/AutoCameraMan/AutoCameraMan.ino
@@ -8,25 +8,27 @@
 #endif
 
 BluetoothSerial SerialBT;
-static const int servoPin = 18; //printed G14 on the board
+static const int servoPin = 18; //printed 18 on the board
+static const int panServoPin = 5; //printed 5 on the board
 Servo_ESP32 servo1;
-int angle =0;
+Servo_ESP32 servo2;
+int panAngle = 0;
+int angle = 0;
 int angleStep = 5;
 int angleMin =0;
 int angleMax = 180;
  
 void setup() {
   Serial.begin(115200);
-  SerialBT.begin("AutoCameraMan Base"); //Bluetooth device name
+  SerialBT.begin("AutoCameraMan"); //Bluetooth device name
   Serial.println("The device started, now you can pair it with bluetooth!");
   servo1.attach(servoPin);
+  servo2.attach(panServoPin);
 }
  
 void loop() {
-  /*servoRight();
-  delay(2000);
+  servoRight();
   servoLeft();
-  delay(2000);*/
   
   if (Serial.available()) {
     SerialBT.write(Serial.read());
@@ -42,7 +44,7 @@ void servoRight()
 {
    for(int angle = 0; angle <= angleMax; angle +=angleStep) {
         servo1.write(angle);
-        Serial.println(angle);
+        //Serial.println(angle);
         delay(20);
     }
 }
@@ -51,7 +53,23 @@ void servoLeft()
 {
   for(int angle = 180; angle >= angleMin; angle -=angleStep) {
         servo1.write(angle);
-        Serial.println(angle);
+        //Serial.println(angle);
+        delay(20);
+    }
+}
+
+void panRight()
+{
+   for(int panAngle = 0; panAngle <= angleMax; panAngle +=angleStep) {
+        servo2.write(panAngle);
+        delay(20);
+    }
+}
+
+void panLeft()
+{
+  for(int panAngle = 180; panAngle >= angleMin; panAngle -=angleStep) {
+        servo2.write(panAngle);
         delay(20);
     }
 }
