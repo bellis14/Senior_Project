539af4424fe4fa2eb940ff00fec6da674a336294
Ben Ellis <ben.ellis1919@gmail.com>
2022-02-16 16:18:59 -0700

    updated servo code

--- hardware_code/AutoCameraMan/AutoCameraMan.ino
+++ hardware_code/AutoCameraMan/AutoCameraMan.ino
@@ -1,58 +1,70 @@
-//www.elegoo.com
+//For a better understanding of the bluetooth code go to https://randomnerdtutorials.com/esp32-bluetooth-classic-arduino-ide/
 
 #include "BluetoothSerial.h"
 #include <Servo_ESP32.h>
-
+#define tiltServoPin 18
+#define panServoPin 5
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
 
 BluetoothSerial SerialBT;
-static const int servoPin = 18; //printed 18 on the board
-static const int panServoPin = 5; //printed 5 on the board
-Servo_ESP32 servo1;
-Servo_ESP32 servo2;
+//static const int tiltServoPin = 18; //printed 18 on the board
+//static const int panServoPin = 5; //printed 5 on the board
+Servo_ESP32 tiltServo, panServo;
 int panAngle = 0;
-int angle = 0;
+//int tiltAngle = 0;
 int angleStep = 5;
-int angleMin =0;
+int angleMin = 0;
 int angleMax = 180;
  
 void setup() {
   Serial.begin(115200);
   SerialBT.begin("AutoCameraMan"); //Bluetooth device name
-  Serial.println("The device started, now you can pair it with bluetooth!");
-  servo1.attach(servoPin);
-  servo2.attach(panServoPin);
+  tiltServo.attach(tiltServoPin); 
+  panServo.attach(panServoPin);
 }
  
 void loop() {
-  servoRight();
-  servoLeft();
+  panRight();
+  panLeft();
+  //tiltUp();
+  //tiltDown();
   
-  if (Serial.available()) {
+  //Send data
+  /*if (Serial.available()) {
     SerialBT.write(Serial.read());
-  }
+  }*/
+  
+  // Receive data
   if (SerialBT.available()) {
-    Serial.write(SerialBT.read());
+    //Serial.write(SerialBT.read());
+    char getstr = SerialBT.read();
+    switch(getstr) {
+      case '0': Serial.println("panRight"); break;
+      case '1': Serial.println("panLeft"); break;
+      case '2': Serial.println("tiltUp"); break;
+      case '3': Serial.println("tiltDown"); break;
+      default:  break;
+    }
   }
   delay(20);
 }
 
 
-void servoRight()
+void tiltUp()
 {
-   for(int angle = 0; angle <= angleMax; angle +=angleStep) {
-        servo1.write(angle);
+   for(int tiltAngle = 0; tiltAngle <= angleMax; tiltAngle +=angleStep) {
+        tiltServo.write(tiltAngle);
         //Serial.println(angle);
         delay(20);
     }
 }
 
-void servoLeft()
+void tiltDown()
 {
-  for(int angle = 180; angle >= angleMin; angle -=angleStep) {
-        servo1.write(angle);
+  for(int tiltAngle = 180; tiltAngle >= angleMin; tiltAngle -=angleStep) {
+        tiltServo.write(tiltAngle);
         //Serial.println(angle);
         delay(20);
     }
@@ -61,7 +73,7 @@ void servoLeft()
 void panRight()
 {
    for(int panAngle = 0; panAngle <= angleMax; panAngle +=angleStep) {
-        servo2.write(panAngle);
+        panServo.write(panAngle);
         delay(20);
     }
 }
@@ -69,7 +81,7 @@ void panRight()
 void panLeft()
 {
   for(int panAngle = 180; panAngle >= angleMin; panAngle -=angleStep) {
-        servo2.write(panAngle);
+        panServo.write(panAngle);
         delay(20);
     }
 }
